6dee65c396ac89565a4c1e9d829df8de7f0285d1
megha <megha@LAPTOP-OQJFNEMO>
2022-03-06 22:24:00 +0530

    first commit

---
+++ my project new/src/module-info.java
@@ -0,0 +1,20 @@
+package myprojectnew.java;
+public class Even_numb
+{
+	
+	public static void main(String[] args)
+	{
+			int number=100;  
+			System.out.println("number");
+			for (int i=1; i<=number; i++)
+			{   
+			if (i%2==0)   
+			{  
+			System.out.print(i + " ");  
+			}  
+			}  
+			}  
+			} 
+	}
+	
+}
