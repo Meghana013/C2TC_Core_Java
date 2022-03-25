8657a48ca387e8ee931c05b823d7991bc7a7d90d
megha <megha@LAPTOP-OQJFNEMO>
2022-03-25 18:18:52 +0530

    Assingment2

---
+++ my project new/src/NameAndVersion.java
@@ -0,0 +1,8 @@
+
+public class NameAndVersion{
+
+		   public static void main(String []args){
+		      System.out.println("OS Name: " + System.getProperty("os.name"));
+		      System.out.println("OS Version: " + System.getProperty("os.version"));
+		   }
+		}
